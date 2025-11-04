ef42c1b7f0f4783af52666e80ead7bd71cb027fe
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-11-04 11:04:18 +0300

    add secret 1

--- src/main/java/ru/otus/danilchenko/algorithms/lesson1/Secrets.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson1/Secrets.java
@@ -4,4 +4,7 @@ public class Secrets {
     public static boolean defaultSecret(int x, int y) {
         return true;
     }
+    public static boolean secret1(int x, int y) {
+        return x > y;
+    }
 }
