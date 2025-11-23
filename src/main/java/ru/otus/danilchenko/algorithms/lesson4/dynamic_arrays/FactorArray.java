73037268be0b8ffa9ed9d6d0f74a554d3cd396aa
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-11-23 10:33:26 +0300

    add factor array

--- src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/FactorArray.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/FactorArray.java
@@ -4,7 +4,7 @@ public class FactorArray<T> implements IArray<T> {
     private static int FACTOR = 2;
     private T[] container;
 
-    FactorArray(int size){
+    FactorArray(int size) {
         container = ArrayUtils.createArray(size);
     }
 
@@ -17,15 +17,24 @@ public class FactorArray<T> implements IArray<T> {
             container[index] = item;
             return;
         }
-
         int diff = index - container.length;
         int multiplyFactor = diff / FACTOR;
-
-        
+        int size = container.length * multiplyFactor == 0 ? 1 : multiplyFactor * FACTOR;
+        T[] newArray = ArrayUtils.createArray(size);
+        ArrayUtils.copyArray(container, newArray);
+        container = newArray;
     }
 
     @Override
     public T remove(int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException("index must be positive");
+        }
+        if (index < container.length) {
+            T result = container[index];
+            container[index] = null;
+            return result;
+        }
         return null;
     }
 }
