defc3a30991ba1e5645e15f7ac52bc60d43a8c7e
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-11-23 20:24:34 +0300

    fix single array

--- src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/SingleArray.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/SingleArray.java
@@ -39,16 +39,29 @@ public class SingleArray<T> implements IArray<T> {
             throw new IllegalArgumentException("index must be positive");
         }
         if (index >= container.length) {
-            return null;
+            throw new IllegalArgumentException("out of range");
         }
         T result = container[index];
-        container[index] = null;
+        T[] newArray = ArrayUtils.createArray(container.length - 1);
+        if (index == 0) {
+            System.arraycopy(container, index + 1, newArray, index, container.length - 1);
+            container = newArray;
+            return result;
+        }
+        if (index == container.length - 1) {
+            System.arraycopy(container, 0, newArray, 0, index);
+            container = newArray;
+            return result;
+        }
+        System.arraycopy(container, 0, newArray, 0, index);
+        System.arraycopy(container, index + 1, newArray, index, newArray.length - index);
+        container = newArray;
         return result;
     }
 
     @Override
     public T get(int index) {
-        if(index < 0){
+        if (index < 0) {
             throw new IllegalArgumentException("index must be positive");
         }
         if (index >= container.length) {
