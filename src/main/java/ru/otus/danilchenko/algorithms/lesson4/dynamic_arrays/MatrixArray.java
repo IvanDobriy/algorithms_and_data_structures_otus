5e6d994db0c0642e5dcb76bca9e9af1ce7ad6bc7
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-11-24 19:07:07 +0300

    prepare matrix array

--- src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/MatrixArray.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/MatrixArray.java
@@ -2,14 +2,13 @@ package ru.otus.danilchenko.algorithms.lesson4.dynamic_arrays;
 
 public class MatrixArray<T> implements IArray<T> {
     private static final int PARTITION_SIZE = 10;
-    private T[][] container;
+    private TwoDimensionallyArray<T> container;
+    private int size;
 
     MatrixArray(int size) {
         int linkSize = size / PARTITION_SIZE + 1;
-        container = ArrayUtils.createArray(linkSize);
-        for (int i = 0; i < container.length; i++) {
-            container[i] = ArrayUtils.createArray(PARTITION_SIZE);
-        }
+        container = new TwoDimensionallyArray<>(linkSize, PARTITION_SIZE);
+        this.size = size;
     }
 
     @Override
@@ -17,18 +16,17 @@ public class MatrixArray<T> implements IArray<T> {
         if (index < 0) {
             throw new IllegalArgumentException("index must be positive");
         }
-        int containerSize = container.length * PARTITION_SIZE;
+        int containerSize = container.getRowSize() * container.getColSize();
         if (index < containerSize) {
-            T[] partition = container[index / PARTITION_SIZE];
-            partition[index % PARTITION_SIZE] = item;
+            container.set(index / PARTITION_SIZE, index % PARTITION_SIZE, item);
             return;
         }
         int factor = index / PARTITION_SIZE;
-        T[][] newContainer = ArrayUtils.createArray(factor + 1);
-        ArrayUtils.copyArray(container, newContainer);
-        T[] partition = ArrayUtils.createArray(PARTITION_SIZE);
-        partition[index % PARTITION_SIZE] = item;
-        newContainer[factor] = partition;
+
+        TwoDimensionallyArray<T> newArray = new TwoDimensionallyArray<>(factor + 1, PARTITION_SIZE);
+        newArray.copy(0, container, 0, container.getRowSize());
+        newArray.set(index / PARTITION_SIZE, index % PARTITION_SIZE, item);
+        container = newArray;
     }
 
     @Override
@@ -36,24 +34,24 @@ public class MatrixArray<T> implements IArray<T> {
         if (index < 0) {
             throw new IllegalArgumentException("index must be positive");
         }
-        int containerSize = container.length * PARTITION_SIZE;
-        if (index >= containerSize) {
-            return null;
-        }
-        T[] partition = container[index / PARTITION_SIZE];
-        int partitionPosition = index % PARTITION_SIZE;
-        T result = partition[partitionPosition];
-        partition[partitionPosition] = null;
-        return result;
-    }
-
-    @Override
-    public T get(int index) {
+//        int containerSize = container.length * PARTITION_SIZE;
+//        if (index >= containerSize) {
+//            return null;
+//        }
+//        T[] partition = container[index / PARTITION_SIZE];
+//        int partitionPosition = index % PARTITION_SIZE;
+//        T result = partition[partitionPosition];
+//        partition[partitionPosition] = null;
         return null;
     }
 
     @Override
+    public T get(int index) {
+        return container.get(index / PARTITION_SIZE, index % PARTITION_SIZE);
+    }
+
+    @Override
     public int size() {
-        return 0;
+        return size;
     }
 }
--- src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/TwoDimensionallyArray.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/TwoDimensionallyArray.java
@@ -1,9 +1,12 @@
 package ru.otus.danilchenko.algorithms.lesson4.dynamic_arrays;
 
+import java.util.Objects;
+
 public class TwoDimensionallyArray<T> {
-    private final int rowSize;
-    private final int colSize;
-    private final Object[][] array;
+    private int rowSize;
+    private int colSize;
+    private Object[][] array;
+
 
     TwoDimensionallyArray(int rowSize, int colSize) {
         if (rowSize < 0) {
@@ -12,7 +15,7 @@ public class TwoDimensionallyArray<T> {
         if (colSize < 0) {
             throw new IllegalArgumentException("col size must be positive");
         }
-        array = new Object[rowSize][colSize];
+        array = new Object[rowSize][0];
         this.rowSize = rowSize;
         this.colSize = colSize;
     }
@@ -24,6 +27,9 @@ public class TwoDimensionallyArray<T> {
         if (colIndex < 0) {
             throw new IllegalArgumentException("col index must positive");
         }
+        if (array[rowIndex].length == 0) {
+            array[rowIndex] = new Object[colSize];
+        }
         array[rowIndex][colIndex] = value;
     }
 
@@ -34,7 +40,26 @@ public class TwoDimensionallyArray<T> {
         if (colIndex < 0) {
             throw new IllegalArgumentException("col index must positive");
         }
+        if (array[rowIndex].length == 0) {
+            array[rowIndex] = new Object[colSize];
+        }
         return (T) array[rowIndex][colIndex];
+    }
+
+    public void copy(int fromRowIndex, TwoDimensionallyArray<T> from, int rowSize, int toRowIndex) {
+        Objects.requireNonNull(from);
+        if (fromRowIndex < 0) {
+            throw new IllegalArgumentException("from row index must be positive");
+        }
+        if (toRowIndex < 0) {
+            throw new IllegalArgumentException("to row index must be positive");
+        }
+        if (rowSize < 0) {
+            throw new IllegalArgumentException("row size must be positive");
+        }
+        for (int i = 0; i < rowSize; i++) {
+            array[toRowIndex + i] = from.array[fromRowIndex];
+        }
     }
 
     public int getRowSize() {
@@ -44,5 +69,4 @@ public class TwoDimensionallyArray<T> {
     public int getColSize() {
         return colSize;
     }
-
 }
