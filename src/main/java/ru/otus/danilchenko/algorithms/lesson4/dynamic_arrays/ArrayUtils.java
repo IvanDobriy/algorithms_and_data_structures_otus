176ed10e0ba50dc00e52860075e072c018deff20
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-11-25 11:15:02 +0300

    delete unused method

--- src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/ArrayUtils.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson4/dynamic_arrays/ArrayUtils.java
@@ -4,13 +4,4 @@ public class ArrayUtils {
     public static <T> T[] createArray(int size) {
         return (T[]) (new Object[size]);
     }
-
-    public static <T> void copyArray(T[] from, T[] to) {
-        if (from.length > to.length) {
-            throw new IllegalArgumentException("from.length must be more than to.length");
-        }
-        for (int i = 0; i < from.length; i++) {
-            to[i] = from[i];
-        }
-    }
 }
