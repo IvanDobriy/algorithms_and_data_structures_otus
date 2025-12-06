978f30c1cb6316e7b31dae78c8c198c133c93e02
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-12-06 18:24:47 +0300

    add bubbleSortTest

--- src/main/java/ru/otus/danilchenko/algorithms/lesson6/App.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson6/App.java
@@ -1,20 +1,47 @@
 package ru.otus.danilchenko.algorithms.lesson6;
 
+import ru.otus.danilchenko.algorithms.test.Test;
+
+import java.io.PrintStream;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class App {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
 
-    private void run(String[] args) {
+    private void bubbleSortTest(String[] inputData, String[] expectedData, PrintStream out) {
+        Integer[] arr = Arrays.stream(inputData[1].split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] expected = Arrays.stream(expectedData[0].split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
         final var metric = new Metric("bubble sort");
         final var utils = new Utils();
         final var comparator = new CompareWithMetic<>(utils::compare, metric);
         final var swapper = new SwapWithMetrics<Integer>(utils::swap, metric);
         final var bubbleSort = new BubbleSort<Integer>(comparator, swapper);
-        final Integer[] arr = {3, 2, 1, 0};
+
         final var result = bubbleSort.sort(arr);
-        logger.info(String.format("%s", Arrays.toString(result)));
+
+        if (!Arrays.deepEquals(expected, result)) {
+            out.println(String.format("Test err"));
+            return;
+        }
+        out.println("Test ok");
+    }
+
+
+    private void run(String[] args) {
+        final var tests = List.of(
+                new Test("Bubble sort",
+                        Paths.get("./test_cases/lesson6/sorting-tests/0.random"),
+                        0, 5,
+                        this::bubbleSortTest
+                )
+        );
+        for(var test: tests) {
+            test.run();
+        }
     }
 
     public static void main(String[] args) {
