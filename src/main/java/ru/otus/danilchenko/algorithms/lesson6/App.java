b7ddd51a2c2537c9b60517dee1b3a3de8860751f
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-12-07 17:06:56 +0300

    add shell sort

--- src/main/java/ru/otus/danilchenko/algorithms/lesson6/App.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson6/App.java
@@ -71,6 +71,32 @@ public class App implements AutoCloseable {
         parameters.getOut().println("Test ok");
     }
 
+    private void shellSortTest(Test.TestRunnerParameters parameters) {
+        int size = Integer.parseInt(parameters.getInputData()[0]);
+        Integer[] arr = Arrays.stream(parameters.getInputData()[1].split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer[] expected = Arrays.stream(parameters.getExpectedData()[0].split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        final var metric = new Metric("Shell sort");
+        final var utils = new Utils();
+        final var comparator = new CompareWithMetic<>(utils::compare, metric);
+        final var swapper = new SwapWithMetrics<Integer>(utils::swap, metric);
+        final var shellSort = new ShellSort<Integer>(comparator, swapper);
+
+        final var result = shellSort.sort(arr);
+
+        final var metricResult = metric.getMetrics();
+        final var compareMetrics = metricResult.get(CompareWithMetic.TAG);
+        final var exchangeMetrics = metricResult.get(SwapWithMetrics.TAG);
+        report.addReportData("shellSort", parameters.getCasePath().toString(),
+                new SortingReportData(size, compareMetrics == null ? 0 : compareMetrics,
+                        exchangeMetrics == null ? 0 : exchangeMetrics));
+        if (!Arrays.deepEquals(expected, result)) {
+            parameters.getOut().println(String.format("Test err"));
+            return;
+        }
+        parameters.getOut().println("Test ok");
+    }
+
     private void run(String[] args) {
         final var tests = List.of(
                 new Test("Bubble sort random",
@@ -112,6 +138,26 @@ public class App implements AutoCloseable {
                         Paths.get("./test_cases/lesson6/sorting-tests/3.revers"),
                         0, 5,
                         this::insertionSortTest
+                ),
+                new Test("Shell sort random",
+                        Paths.get("./test_cases/lesson6/sorting-tests/0.random"),
+                        0, 5,
+                        this::shellSortTest
+                ),
+                new Test("Shell sort digits",
+                        Paths.get("./test_cases/lesson6/sorting-tests/1.digits"),
+                        0, 5,
+                        this::shellSortTest
+                ),
+                new Test("Shell sort sorted",
+                        Paths.get("./test_cases/lesson6/sorting-tests/2.sorted"),
+                        0, 5,
+                        this::shellSortTest
+                ),
+                new Test("Shell sort revers",
+                        Paths.get("./test_cases/lesson6/sorting-tests/3.revers"),
+                        0, 5,
+                        this::shellSortTest
                 )
         );
         for (var test : tests) {
@@ -121,8 +167,11 @@ public class App implements AutoCloseable {
 
     @Override
     public void close() throws Exception {
-        report.build();
-        report.close();
+        try {
+            report.build();
+        } finally {
+            report.close();
+        }
     }
 
     public static void main(String[] args) {
