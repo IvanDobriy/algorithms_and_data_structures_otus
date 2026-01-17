37b8d1ab47cffb6a81e5255f16f4f9ae117c78bb
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-01-17 19:12:00 +0300

    prepare BinarySearchTree

--- src/main/java/ru/otus/danilchenko/algorithms/lesson10/BinarySearchTree.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson10/BinarySearchTree.java
@@ -47,6 +47,7 @@ public class BinarySearchTree<T> implements ITree<T> {
 
     @Override
     public void insert(T value) {
+        Objects.requireNonNull(value);
         if (root == null) {
             root = new Node(value);
             return;
@@ -71,6 +72,7 @@ public class BinarySearchTree<T> implements ITree<T> {
 
     @Override
     public boolean search(T value) {
+        Objects.requireNonNull(value);
         if (root == null) {
             return false;
         }
@@ -93,21 +95,82 @@ public class BinarySearchTree<T> implements ITree<T> {
         } while (true);
     }
 
+
+    private Node findParent(T value) {
+        Objects.requireNonNull(value);
+        if (root == null) {
+            return null;
+        }
+        Node current = root;
+        Node previous = null;
+        do {
+            if (comparator.compare(value, current.key) == 0) {
+                return previous;
+            }
+            if (comparator.compare(value, current.key) < 0) {
+                if (current.left == null) {
+                    return null;
+                }
+                previous = current;
+                current = current.left;
+                continue;
+            }
+            if (current.right == null) {
+                return null;
+            }
+            previous = current;
+            current = current.right;
+        } while (true);
+    }
+
     @Override
     public void remove(T value) {
+        Objects.requireNonNull(value);
         if (root == null) {
             return;
         }
 
-//        if (root.key == value) {
-//
-//        }
-//
-//        Node current = root;
-//        Node previous = null;
-//        do {
-//            if (comparator.compare())
-//        } while (true);
+        Node parent = findParent(value);
+        Node current = null;
+
+        if (parent == null) {
+            current = root;
+        } else if (parent.left != null && comparator.compare(parent.left.key, value) == 0) {
+            current = parent.left;
+        } else if (parent.right != null && comparator.compare(parent.right.key, value) == 0) {
+            current = parent.right;
+        }
+
+        if (current == null) {
+            return;
+        }
+        if (current.right == null) {
+            if (current == root) {
+                root = current.left;
+                return;
+            }
+            parent.left = current.left;
+            return;
+        }
+
+        Node minParent = current.right;
+        Node minNode = minParent.left;
+        if (minNode == null) {
+            parent.right = minParent;
+            return;
+        }
+        while (minNode.left != null) {
+            minParent = minNode;
+            minNode = minNode.left;
+        }
+
+
+        if (comparator.compare(parent.left.key, value) == 0) {
+            parent.left = minNode;
+        } else {
+            parent.right = minNode;
+        }
+        minParent.left = minNode.right;
     }
 
     @Override
