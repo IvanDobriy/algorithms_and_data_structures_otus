85384e8df91af0cedd9100c0fc49fbb194491a87
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-03-24 10:24:59 +0300

    prepare alg

---
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson17/App.java
@@ -0,0 +1,8 @@
+package ru.otus.danilchenko.algorithms.lesson17;
+
+public class App {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson17/search/DijkstraAdjacencyVector.java
@@ -0,0 +1,107 @@
+package ru.otus.danilchenko.algorithms.lesson17.search;
+
+import ru.otus.danilchenko.algorithms.lesson14.performance.adjacency_vector.AdjacencyVector;
+import ru.otus.danilchenko.algorithms.lesson16.sceleton.Edge;
+import ru.otus.danilchenko.algorithms.lesson4.dynamic_arrays.IArray;
+import ru.otus.danilchenko.algorithms.lesson4.dynamic_arrays.SingleArray;
+import ru.otus.danilchenko.algorithms.sort.IComparator;
+
+import java.util.Objects;
+
+public class DijkstraAdjacencyVector implements IDijkstra {
+    private final AdjacencyVector adjacencyVector;
+    private IArray<IArray<Edge>> paths;
+    private IComparator<Way> wayIComparator;
+    private final int max;
+
+    private class Way {
+        int from;
+        int weight;
+
+        Way(int from, int weight) {
+            this.from = from;
+            this.weight = weight;
+        }
+    }
+
+    public DijkstraAdjacencyVector(AdjacencyVector adjacencyVector) {
+        Objects.requireNonNull(adjacencyVector);
+        this.adjacencyVector = adjacencyVector;
+        paths = null;
+        max = findMax();
+        wayIComparator = (Way first, Way second) -> {
+            return first.weight - second.weight;
+        };
+    }
+
+    private void check(int from, int to) {
+
+    }
+
+
+    private int findMax() {
+        int result = 0;
+        for (int i = 0; i < adjacencyVector.getVertexSize(); i++) {
+            for (int j = 0; j < adjacencyVector.getMaxAdjacencyDegree(); j++) {
+                var v = adjacencyVector.get(i, j);
+                if (v <= i) {
+                    continue;
+                }
+                var w = adjacencyVector.get(i, j);
+                result += w;
+            }
+        }
+        return result;
+    }
+
+    private int getMinIndex(IArray<Way> ways, boolean[] visited) {
+        int minIndex = -1;
+        for (int i = 0; i < ways.size(); i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if (minIndex < 0) {
+                minIndex = i;
+                continue;
+            }
+            if (wayIComparator.compare(ways.get(i), ways.get(minIndex)) < 0) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+
+    @Override
+    public IArray<Edge> execute(int from, int to) {
+        check(from, to);
+        IArray<Way> ways = new SingleArray<>(adjacencyVector.getVertexSize());
+        for(int i = 0; i < ways.size(); i++){
+            ways.set(new Way(i, max), i);
+        }
+        boolean[] visited = new boolean[adjacencyVector.getVertexSize()];
+        int min  = 0;
+        ways.set(new Way(min, 0), min);
+        for (int i = 0; i < adjacencyVector.getVertexSize(); i++) {
+            min = getMinIndex(ways, visited);
+            visited[min] = true;
+            for (int j = 0; j < adjacencyVector.getMaxAdjacencyDegree(); j++) {
+                if (visited[i]) {
+                    continue;
+                }
+                int av = adjacencyVector.get(i, j);
+                if (av < 0) {
+                    continue;
+                }
+                int weight = ways.get(min).weight + adjacencyVector.getWeight(min, j);
+
+                if(weight < ways.get(j).weight){
+                    
+                }
+
+            }
+        }
+
+        return null;
+    }
+}
