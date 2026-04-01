3970973f3a4d02e67b4b480ad66c8b772f304738
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-04-01 18:17:15 +0300

    add searchFullScanPostfix

--- src/main/java/ru/otus/danilchenko/algorithms/lesson18/App.java
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson18/App.java
@@ -1,12 +1,14 @@
 package ru.otus.danilchenko.algorithms.lesson18;
 
 import ru.otus.danilchenko.algorithms.lesson18.search.SearchFullScan;
+import ru.otus.danilchenko.algorithms.lesson18.search.SearchFullScanPostfixShift;
+import ru.otus.danilchenko.algorithms.lesson18.search.SearchFullScanPrefixShift;
 
 public class App {
     private final String text;
 
     App() {
-        text = "hello world";
+        text = "hello wworworld";
     }
 
     private int searchFullScan(String pattern) {
@@ -14,9 +16,21 @@ public class App {
         return sfs.search(text);
     }
 
+    private int searchFullScanPrefix(String pattern){
+        final SearchFullScanPrefixShift sfsps = new SearchFullScanPrefixShift(pattern);
+        return sfsps.search(text);
+    }
+
+    private int searchFullScanPostfix(String pattern){
+        final SearchFullScanPostfixShift search = new SearchFullScanPostfixShift(pattern);
+        return search.search(text);
+    }
+
+
+
     public static void main(String[] args) {
         App app = new App();
-        int pos = app.searchFullScan("world");
+        int pos = app.searchFullScanPostfix("world");
         int a = pos;
     }
 }
---
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson18/search/SearchFullScanPrefixShift.java
@@ -0,0 +1,59 @@
+package ru.otus.danilchenko.algorithms.lesson18.search;
+
+import ru.otus.danilchenko.algorithms.collections.ITree;
+import ru.otus.danilchenko.algorithms.lesson11.Treap;
+import ru.otus.danilchenko.algorithms.lesson11.TreapTool;
+import ru.otus.danilchenko.algorithms.sort.IComparator;
+
+import java.util.Objects;
+
+public class SearchFullScanPrefixShift implements ISearch {
+    private class Node {
+        char el;
+        int shift;
+
+        Node(char el, int shift) {
+            this.el = el;
+            this.shift = shift;
+        }
+    }
+
+    private final String pattern;
+    private final ITree<Node> tree;
+
+
+    public SearchFullScanPrefixShift(String pattern) {
+        Objects.requireNonNull(pattern);
+        this.pattern = pattern;
+        IComparator<Node> comparator = (Node el1, Node el2) -> {
+            return el1.el - el2.el;
+        };
+        tree = new Treap<>(new TreapTool<>(comparator));
+        for(int i = 0; i < pattern.length(); i++){
+            tree.insert(new Node(pattern.charAt(i), 1));
+        }
+    }
+
+    @Override
+    public int search(String text) {
+        Objects.requireNonNull(text);
+        int pos = 0;
+        int tab = 0;
+        Node node;
+        while (tab <= text.length() - pattern.length()) {
+            while ((pos < pattern.length()) && (pattern.charAt(pos) == text.charAt(pos + tab))) {
+                pos++;
+            }
+            if (pos == pattern.length()) {
+                return tab;
+            }
+            node = tree.searchWithValue(new Node(text.charAt(tab + pattern.length() -1), 0));
+            if (node != null) {
+                tab += node.shift;
+            } else {
+                tab += pattern.length();
+            }
+        }
+        return -1;
+    }
+}
