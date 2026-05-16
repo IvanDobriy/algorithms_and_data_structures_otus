d2da754e8150b6fd9736e41c4786efc172904b66
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-16 09:03:33 +0300

    prepare tests

---
+++ src/main/java/ru/otus/danilchenko/algorithms/lesson21/App.java
@@ -0,0 +1,33 @@
+package ru.otus.danilchenko.algorithms.lesson21;
+
+import ru.otus.danilchenko.algorithms.test.Test;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    private void kingTest(Test.TestRunnerParameters parameters) {
+        parameters.getOut().println("Test ok" + Arrays.toString(parameters.getInputData()) + " out: " + Arrays.toString(parameters.getExpectedData()));
+    }
+
+    private void run(String[] args) {
+        final String defaultCaseName = "1";
+        final var tests = List.of(new Test(
+                "peas",
+                defaultCaseName,
+                Paths.get("./test_cases/lesson21/peas"),
+                0, Integer.MAX_VALUE,
+                this::kingTest
+        ));
+
+        for (var test : tests) {
+            test.run();
+        }
+    }
+
+    public static void main(String[] args) {
+        final App app = new App();
+        app.run(args);
+    }
+}
